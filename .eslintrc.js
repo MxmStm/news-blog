de25ec9ce8abf3149f9575fcee126fbf712e86cb
Maxim <maksstoma@gmail.com>
2022-10-04 18:10:29 +0300

    added error boundaries, added BundleAnalyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 1,
         'no-underscore-dangle': 0,
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
