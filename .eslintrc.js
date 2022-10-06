5c8ea10e2e0497c129a420a6b517c3627a414331
Maxim <maksstoma@gmail.com>
2022-10-06 22:16:51 +0300

    added react testing library; added tests for button, sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,10 +38,22 @@ module.exports = {
         'import/extensions': 0,
         'import/no-extraneous-dependencies': 1,
         'no-underscore-dangle': 0,
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'i18next/no-literal-string': [
+            2,
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid'],
+            },
+        ],
+        'max-len': [2, { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.tsx'],
+            rules: { 'i18next/no-literal-string': 0 },
+        },
+    ],
 };
--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
     // All imported modules in your tests should be mocked automatically
     // automock: false,
@@ -29,6 +31,10 @@ export default {
         'node_modules',
     ],
 
+    modulePaths: [
+        '<rootDir>src',
+    ],
+
     // An array of file extensions your modules use
     moduleFileExtensions: [
         'js',
@@ -41,6 +47,15 @@ export default {
 
     // The root directory that Jest should scan for tests and modules within
     rootDir: '../../',
+
+    setupFilesAfterEnv: [
+        '<rootDir>config/jest/setupTests.ts',
+    ],
+
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
 
     // The glob patterns Jest uses to detect test files
     testMatch: [
